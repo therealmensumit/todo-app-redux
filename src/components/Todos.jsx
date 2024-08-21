73f7c7ad69e3cf5dd07cec8387ac585bf5a90576
therealmensumit <sk03950@gmail.com>
2024-08-21 15:01:58 +0530

    "Updated Todos component to display 'No data found' message when todo list is empty, and modified todoSlice initial state to use nanoid for todo id"

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -6,24 +6,29 @@ import { Button } from "./UI";
 export const Todos = () => {
   const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
+  // console.log(todos);
 
   return (
     <div className="w-full lg:w-3/5">
       <h3 className="mb-3 text-center font-semibold text-2xl">Todo List</h3>
-      <ul className="grid gap-4">
-        {todos?.map((todo) => (
-          <li
-            key={todo.id}
-            className="flex items-center p-3 rounded bg-slate-800 text-white">
-            <div className="flex-1 text-lg">{todo.text}</div>
-            <Button
-              onClick={() => dispatch(removeTodo(todo.id))}
-              className="bg-red-600 hover:bg-red-700 rounded">
-              <FaDeleteLeft />
-            </Button>
-          </li>
-        ))}
-      </ul>
+      {todos.length === 0 ? (
+        <p className="text-center">No data found</p>
+      ) : (
+        <ul className="grid gap-4">
+          {todos?.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex items-center p-3 rounded bg-slate-800 text-white">
+              <div className="flex-1 text-lg">{todo.text}</div>
+              <Button
+                onClick={() => dispatch(removeTodo(todo.id))}
+                className="bg-red-600 hover:bg-red-700 rounded">
+                <FaDeleteLeft />
+              </Button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
